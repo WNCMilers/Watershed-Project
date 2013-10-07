40d2d237e95ee1644187c4732ba33c74fc20a21a
samanthakatzer <sam@radialogica.com>
2013-10-07 16:36:56 -0500

    Added the web project
    These files need to be added to the root folder
    Includes automated email response (need to enter user name and password
    for gmail account)

---
+++ Web/add these php files to the root folder/thanks.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+
+<?php $title="Membership" ?>
+<?php include("#top.htm");?>
+
+<!-- begin blog content -->
+<section id="blog" class="container_12 clearfix">
+
+	<!-- left column -->
+	<div class="grid_8">
+
+		<!-- featured post -->
+		<div class="post featured">
+              <div style="clear: both; height: 1px;">
+              </div>
+	</div>
+	<!-- / left column -->
+
+	<!-- sidebar -->
+	<aside id="sidebar" class="blog grid_4">
+	<H4><b>Thank you for your registration!</b></H4>
+        <br><br><br><br>
+	</aside>
+	<!-- / sidebar -->
+        </div>
+</section>
+<!-- / blog content -->
+
+<?php include("#bottom.htm"); ?>
